0d826ac063e7587aff3d46856164939c65ef1fd3
John Clyde <johnclyde@gmail.com>
2023-06-26 18:55:15 -0700

    Update script.js

--- script.js
+++ script.js
@@ -11,113 +11,122 @@ for (var i = 0; i < 100; i++) {
  * as plain text and then turn them into array like this.
  */
 var theSekitori = [
-  "Y1e Terunofuji 14-1 Y",
-  "O1w Takakeisho 8-7",
-  "S1e Kiribayama 11-4 G",
-  "S1w Hoshoryu 11-4",
-  "S2e Daieisho 10-5",
-  "S2w Wakamotoharu 10-5 G",
-  "K1e Kotonowaka 8-7",
-  "K1w Wakatakakage 0-0-15",
-  "K2e Shodai 6-9",
-  "M1e Abi 8-7",
-  "M1w Midorifuji 6-9",
-  "M2e Takayasu 3-3-9",
-  "M2w Endo 0-7-8",
-  "M3e Tobizaru 8-7",
-  "M3w Nishikifuji 3-12",
-  "M4e Ura 7-8",
-  "M4w Nishikigi 9-6",
-  "M5e Kinbozan 4-11",
-  "M5w Kotoshoho 2-10-3",
-  "M6e Meisei 8-7 S",
-  "M6w Mitakeumi 9-6",
-  "M7e Hokutofuji 6-9",
-  "M7w Tamawashi 7-8",
-  "M8e Sadanoumi 7-8",
-  "M8w Takanosho 7-8",
-  "M9e Onosho 8-7",
-  "M9w Hiradoumi 9-6",
-  "M10e Ryuden 5-10",
-  "M10w Takarafuji 5-10",
-  "M11e Hokuseiho 8-7",
-  "M11w Daishoho 6-9",
-  "M12e Aoiyama 5-10",
-  "M12w Kotoeko 8-7",
-  "M13e Chiyoshoma 8-7",
-  "M13w Ichinojo 0-0",
-  "M14e Asanoyama 12-3 J",
-  "M14w Myogiryu 9-6",
-  "M15e Ichiyamamoto 4-11",
-  "M15w Tsurugisho 9-6",
-  "M16e Mitoryu 5-10",
-  "M16w Oho 11-4",
-  "M17e Kagayaki 7-8",
-  "J1e Gonoyama 14-1 Y",
-  "J1w Shonannoumi 11-4",
-  "J2e Azumaryu 2-13",
-  "J2w Tohakuryu 4-11",
-  "J3e Bushozan 10-5",
-  "J3w Enho 0-10-5",
-  "J4e Oshoma 7-8",
-  "J4w Roga 9-6",
-  "J5e Tochinoshin 0-6",
-  "J5w Akua 5-10",
-  "J6e Hakuyozan 6-8-1",
-  "J6w Shimazuumi 8-7",
-  "J7e Takakento 6-9",
-  "J7w Churanoumi 7-8",
-  "J8e Atamifuji 13-2",
-  "J8w Ochiai 14-1 D",
-  "J9e Daiamami 7-8",
-  "J9w Chiyonokuni 0-10-5",
-  "J10e Chiyomaru 6-9",
-  "J10w Kitanowaka 8-7",
-  "J11e Hidenoumi 6-9",
-  "J11w Tamashoho 9-6",
-  "J12e Tomokaze 8-7",
-  "J12w Chiyosakae 8-7",
-  "J13e Fujiseiun 9-6",
-  "J13w Shimanoumi 8-7",
-  "J14e Tokihayate 6-9",
-  "J14w Tsushimanada 8-7",
-  "Ms1e Shiden 4-3",
-  "Ms1w Kawazoe 5-2",
-  "Ms2e Kotokuzan 3-4",
-  "Ms2w Shishi 6-1",
-  "Ms3e Ryuo 1-7",
-  "Ms3w Chiyonoumi 4-3",
-  "Ms4e Tochimusashi 4-3",
-  "Ms4w Hayatefuji 3-4",
-  "Ms5e Yuma 5-2",
-  "Ms5w Terutsuyoshi 2-5",
-  "Ms6e Mukainakano 4-3",
-  "Ms6w Tokushoryu 2-5",
-  "Ms7e Dewanoryu 2-5",
-  "Ms7w Akiseyama 2-5",
-  "Ms8e Kamito 3-4",
-  "Ms8w Kanzaki 3-4",
-  "Ms9e Nabatame 3-4",
-  "Ms9w Kaisho 4-3",
-  "Ms10e Nishinoryu 3-4",
-  "Ms10w Tsukahara 4-3",
-  "Ms10TD Onosato 6-1",
-  "Ms11e Ishizaki 5-2",
-  "Ms11w Miyagi 3-4",
-  "Ms12e Yoshii 2-5",
-  "Ms12w Mineyaiba 4-3",
-  "Ms13e Takahashi 6-1",
-  "Ms13w Kazekeno 5-2",
-  "Ms14e Kotoozutsu 4-3",
-  "Ms14w Chiyonoo 5-2",
-  "Ms15e Tochikamiyama 2-5",
-  "Ms15w Toshunryu 3-4",
+  "Y1e Terunofuji 1-3-11",
+  "O1e Takakeisho 0-0-15",
+  "O1w Kirishima 6-5-2",
+  "S1e Hoshoryu 10-3",
+  "S1w Daieisho 8-5",
+  "S2w Wakamotoharu 9-4",
+  "K1e Kotonowaka 9-4",
+  "K1w Abi 4-9",
+  "M1e Nishikigi 10-3",
+  "M1w Tobizaru 7-6",
+  "M2e Shodai 5-8",
+  "M2w Mitakeumi 2-11",
+  "M3e Midorifuji 3-10",
+  "M3w Meisei 6-7",
+  "M4e Asanoyama 6-4-3",
+  "M4w Ura 6-7",
+  "M5e Hiradoumi 5-8",
+  "M5w Onosho 6-7",
+  "M6e Hokuseiho 5-8",
+  "M6w Oho 5-8",
+  "M7e Takayasu 6-7",
+  "M7w Tamawashi 8-5",
+  "M8e Sadanoumi 4-9",
+  "M8w Nishikifuji 5-8",
+  "M9e Takanosho 7-6",
+  "M9w Hokutofuji 11-2",
+  "M10e Kinbozan 6-7",
+  "M10w Myogiryu 6-7",
+  "M11e Kotoeko 7-6",
+  "M11w Tsurugisho 4-9",
+  "M12e Chiyoshoma 6-7",
+  "M12w Wakatakakage 0-0-15",
+  "M13e Gonoyama 8-5",
+  "M13w Kotoshoho 5-8",
+  "M14e Daishoho 4-9",
+  "M14w Shonannoumi 9-4",
+  "M15e Ryuden 9-4",
+  "M15w Takarafuji 8-5",
+  "M16e Endo 9-4",
+  "M16w Bushozan 3-10",
+  "M17e Aoiyama 7-6",
+  "M17w Hakuoho 10-3",
+  "J1e Kagayaki 8-5",
+  "J1w Atamifuji 10-3",
+  "J2e Roga 8-5",
+  "J2w Mitoryu 6-7",
+  "J3e Ichiyamamoto 4-7-2",
+  "J3w Shimazuumi 5-8",
+  "J4e Oshoma 6-7",
+  "J4w Tohakuryu 6-7",
+  "J5e Tamashoho 8-5",
+  "J5w Kitanowaka 8-5",
+  "J6e Fujiseiun 0-0-15",
+  "J6w Azumaryu 6-7",
+  "J7e Hakuyozan 5-8",
+  "J7w Churanoumi 6-7",
+  "J8e Takakento 5-8",
+  "J8w Akua 6-7",
+  "J9e Daiamami 9-4",
+  "J9w Tomokaze 10-3",
+  "J10e Chiyosakae 7-6",
+  "J10w Shimanoumi 5-8",
+  "J11e Chiyomaru 7-6",
+  "J11w Tsushimanada 2-11",
+  "J12e Shishi 8-5",
+  "J12w Hidenoumi 5-8",
+  "J13e Kiho 8-5",
+  "J13w Shiden 7-6",
+  "J14e Yuma 4-9",
+  "J14w Chiyonoumi 4-9",
+  "Ms1e Tokihayate 7-0",
+  "Ms1w Enho 0-0-7",
+  "Ms2e Tochimusashi 1-5",
+  "Ms2w Kiryuko 3-4",
+  "Ms3e Onosato 3-3",
+  "Ms3w Mukainakano 3-3",
+  "Ms4e Kotokuzan 2-5",
+  "Ms4w Takahashi 4-2",
+  "Ms5e Ishizaki 5-2",
+  "Ms5w Chiyonokuni 0-0-7",
+  "Ms6e Kazekeno 3-3",
+  "Ms6w Kaisho 3-3",
+  "Ms7e Hayatefuji 3-2-2",
+  "Ms7w Tsukahara 6-1",
+  "Ms8e Chiyonoo 2-4",
+  "Ms8w Mineyaiba 2-4",
+  "Ms9e Otsuji 2-4",
+  "Ms9w Hitoshi 4-2",
+  "Ms10e Kotoozutsu 2-4",
+  "Ms10w Kamito 4-2",
+  "Ms11e Kanzaki 2-4",
+  "Ms11w Nabatame 4-2",
+  "Ms12e Terutsuyoshi 4-2",
+  "Ms12w Fukai 3-3",
+  "Ms13e Yago 3-4",
+  "Ms13w Fujinoyama 3-3",
+  "Ms14e Nishinoryu 2-4",
+  "Ms14w Tsurubayashi 1-5",
+  "Ms15e Tokushoryu 1-5",
+  "Ms15w Dewanoryu 3-3",
+  "Ms16e Akiseyama 5-2",
+  "Ms16w Oshoumi 4-3",
+  "Ms17e Takerufuji 5-1",
+  "Ms17w Hokaho 1-5",
+  "Ms18e Kototebakari 5-2",
+  "Ms18w Daishomaru 2-5",
+  "Ms19e Wakatakamoto 4-3",
+  "Ms19w Hatsuyama 5-1",
+  "Ms20e Ryuo 4-3",
+  "Ms20w Miyagi 3-3",
 ];
 
 /* Add here the shikona of retired sekitori, who will not appear in the
  * following banzuke. If nobody retired then leave this array empty
  */
-var retiredRikishi = ["Ichinojo", "Tochinoshin"];
+var retiredRikishi = [];
 
 /* Enable "No Rank Colouring" and "One Column" options and then open the
  * browser's inspector (F12). Find the table and copy & paste the <tbody> node.
@@ -1940,270 +1949,7 @@ function exportTableToCSV($table, filename) {
 }
 
 window.onload = function () {
-  var basho = "202305"; // The date of the basho just ended
-
-  /*
-  var CLIENT_ID = "527214845927-p6ofscooll9ettfc8vpb4f5dqbhome4h.apps.googleusercontent.com";
-  var API_KEY = "AIzaSyBiIfRASPUPjYmDLggGBQKCw63h-5B073o";
-  var DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/drive/v3/rest"];
-  // https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile openid https://www.googleapis.com/auth/drive.appdata https://www.googleapis.com/auth/drive.appfolder https://www.googleapis.com/auth/drive.install
-  var SCOPES = "https://www.googleapis.com/auth/drive.file https://www.googleapis.com/auth/drive.resource";
-  var signinButton = document.getElementById("signinButton");
-  var signoutButton = document.getElementById("signoutButton");
-  var saveToDriveButton = document.getElementById("saveToDrive");
-  var loadSaveButton = document.getElementById("loadSave");
-  var messageLine = document.getElementById("messageLine");
-  var progressText = document.getElementById("progressText");
-  let tokenClient;
-  let gapiInited = false;
-  let gisInited = false;
-
-  signinButton.style.display = "none";
-  signoutButton.style.display = "none";
-  saveToDriveButton.style.display = "none";
-  loadSaveButton.style.display = "none";
-
-  gapiLoaded();
-  gisLoaded();
-
-  function gapiLoaded() {
-    gapi.load("client", initializeGapiClient);
-  }
-
-  async function initializeGapiClient() {
-    await gapi.client.init({
-      apiKey: API_KEY,
-      discoveryDocs: DISCOVERY_DOCS
-    });
-    gapiInited = true;
-    maybeEnableButtons();
-  }
-
-  function gisLoaded() {
-    tokenClient = google.accounts.oauth2.initTokenClient({
-      client_id: CLIENT_ID,
-      scope: SCOPES, 
-      prompt: "", 
-      callback: ""
-    });
-    gisInited = true;
-    maybeEnableButtons();
-  }
-
-  function maybeEnableButtons() {
-    if (gapiInited && gisInited) {
-      signinButton.style.display = "inline-block";
-      messageLine.innerHTML = "Save or load your banzuke via Google Drive";
-      progressText.innerHTML = "";
-    }
-  }
-
-  signinButton.onclick = () => handleAuthClick()
-  function handleAuthClick() {
-    tokenClient.callback = async (resp) => {
-      if (resp.error !== undefined) 
-        throw (resp);
-      signinButton.style.display = "none";
-      signoutButton.style.display = "inline-block";
-      saveToDriveButton.style.display = "inline-block";
-      loadSaveButton.style.display = "inline-block";
-      messageLine.innerHTML = "Please wait...";
-      checkFolder("GTB Helper Save (do not modify)");
-    };
-
-    if (gapi.client.getToken() === null) 
-      tokenClient.requestAccessToken({ prompt: "consent" });
-    else 
-      tokenClient.requestAccessToken({ prompt: "" });
-  }
-
-  signoutButton.onclick = () => handleSignoutClick()
-  function handleSignoutClick() {
-    const token = gapi.client.getToken();
-
-    if (token !== null) {
-      google.accounts.oauth2.revoke(token.access_token);
-      gapi.client.setToken("");
-      signinButton.style.display = "inline-block";
-      signoutButton.style.display = "none";
-      saveToDriveButton.style.display = "none";
-      loadSaveButton.style.display = "none";
-      messageLine.innerHTML = "Save or load your banzuke via Google Drive";
-      progressText.innerHTML = "";
-    }
-  }
-
-  function checkFolder(folderName) {
-    gapi.client.drive.files.list({
-      'q': "name = '" + folderName + "'"
-    }).then(function (response) {
-      var files = response.result.files;
-
-      if (files && files.length > 0) {
-        for (var i = 0; i < files.length; i++) {
-          var file = files[i];
-
-          window.localStorage.setItem("backupFolderId", file.id);
-          //console.log("Folder available");
-          showSave();
-        }
-      }
-      else {
-        createFolder("GTB Helper Save (do not modify)");
-        messageLine.innerHTML = "No save";
-        loadSaveButton.disabled = true;
-      }
-    });
-  }
-
-  function createFolder(folderName) {
-    var access_token = gapi.auth.getToken().access_token;
-    var request = gapi.client.request({
-      "path": "drive/v2/files",
-      "method": "POST",
-      "headers": {
-        "Content-Type": "application/json", 
-        "Authorization": "Bearer " + access_token
-      }, 
-      "body": {
-        "title": folderName, 
-        "mimeType": "application/vnd.google-apps.folder"
-      }
-    });
-    request.execute(function (response) {
-      window.localStorage.setItem("backupFolderId", response.id);
-    })
-  }
-
-  function showSave() {
-    var saveId = "";
-
-    gapi.client.drive.files.list({
-      'q': "name = 'gtb_helper_save.txt' and parents in '" + 
-           window.localStorage.getItem("backupFolderId") + "'"
-    }).then(function (response) {
-      var files = response.result.files;
-
-      if (files && files.length > 0) {
-        for (var i = 0; i < files.length; i++) {
-          var saveId = files[i].id;
-
-          gapi.client.drive.files.get({
-            "fileId": saveId, 
-            "fields": "modifiedTime"
-          }).then(function (res) {
-            var modifiedTime = moment(res.result.modifiedTime, 
-              "YYYY-MM-DDThh:mm:ss.SSSZ").format("dddd, MMMM Do YYYY, h:mm:ss a");
-
-            messageLine.setAttribute("data-saveId", saveId);
-            messageLine.innerHTML = "From " + modifiedTime;
-            loadSaveButton.disabled = false;
-          });
-        }
-      }
-      else {
-        loadSaveButton.disabled = true;
-        messageLine.innerHTML = "No save";
-      }
-    })
-  }
-
-  function uploadSave() {
-    const blob = new Blob([window.localStorage.getItem("picks")], { type: "plain/text" });
-    const parentFolder = window.localStorage.getItem("backupFolderId");
-    var metadata = {
-      name: "gtb_helper_save.txt", 
-      mimeType: "plain/text", 
-      parents: [parentFolder]
-    };
-    var formData = new FormData();
-
-    formData.append("metadata", new Blob([JSON.stringify(metadata)], { type: "application/json" }));
-    formData.append("file", blob);
-
-    fetch("https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart", {
-      method: "POST", 
-      headers: new Headers({ "Authorization": "Bearer " + gapi.auth.getToken().access_token }), 
-      body: formData
-    }).then(function (response) {
-      if (response.ok) {
-        progressText.innerHTML = "Saved to Drive!";
-        showSave();
-        setTimeout(function() {
-          progressText.innerHTML = "";
-        }, 1000);
-      }
-      else {
-        console.error(response);
-        progressText.innerHTML = "Access token expired. Please sign out and try again";
-      }
-      //return response.json();
-    }).catch(function(err) {});
-  }
-
-  function updateSave() {
-    var saveId = messageLine.getAttribute("data-saveId");
-    var url = "https://www.googleapis.com/upload/drive/v3/files/" + saveId + "?uploadType=media";
-    
-    fetch(url, {
-      method: "PATCH",
-      headers: new Headers({
-        Authorization: "Bearer " + gapi.auth.getToken().access_token,
-        "Content-type": "plain/text; charset=UTF-8"
-      }), 
-      body: window.localStorage.getItem("picks")
-    }).then(function (response) {
-      if (response.ok) {
-        progressText.innerHTML = "Saved to Drive!";
-        showSave();
-        setTimeout(function() {
-          progressText.innerHTML = "";
-        }, 1000);
-      }
-      else {
-        console.error(response);
-        progressText.innerHTML = "Access token expired. Please sign out and try again";
-      }
-    }).catch(function(err) {});
-  }
-
-  function b64_to_utf8(str) {
-    return decodeURIComponent(escape(window.atob(str)));
-  }
-
-  saveToDriveButton.addEventListener("click", function() {
-    if (window.localStorage.getItem("picks") !== null) {
-      progressText.innerHTML = "Please wait...";
-
-      if (messageLine.innerHTML == "No save") 
-        uploadSave();
-      else 
-        updateSave();
-    }
-  });
-
-  loadSaveButton.addEventListener("click", function() {
-    var saveId = messageLine.getAttribute("data-saveId");
-
-    progressText.innerHTML = "Please wait...";
-
-    gapi.client.drive.files.get({
-      fileId: saveId, 
-      alt: "media"
-    }).then(function (res) {
-      var banzukeHtml = b64_to_utf8(btoa(res.body));
-
-      document.getElementById("tableLiner").innerHTML = banzukeHtml;
-      window.localStorage.setItem("picks", banzukeHtml);
-      redips.init();
-      progressText.innerHTML = "";
-    }).catch(function (err) {
-      console.error(err);
-      progressText.innerHTML = "Access token expired. Please sign out and try again";
-    });
-  });
-  */
-
+  var basho = "202307"; // The date of the basho just ended
   //****************************************************************************
 
   // This must be a hyperlink
@@ -2963,57 +2709,3 @@ function showSaving() {
 if (window.addEventListener)
   window.addEventListener("load", redips.init, false);
 else if (window.attachEvent) window.attachEvent("onload", redips.init);
-
-/*
-var hoshitori = [], 
-    rikishiTr = document.getElementsByTagName("tr");
-
-for (var i = 1; i < rikishiTr.length; i++) {
-  var recordArr = [], 
-    aiteArr = [], 
-    hyper = rikishiTr[i].getElementsByTagName('a');
-
-  for (var j = 1; j < hyper.length; j++) {
-    aiteArr.push(hyper[j].title.split(' ')[2]);
-    if (hyper[j].children[0].getAttribute("src") == "img/w.gif") {
-      if (hyper[j].title.split(' ')[3] == "fusen") 
-        recordArr.push(3);
-      else 
-        recordArr.push(1);
-    }
-    else {
-      if (hyper[j].title.split(' ')[3] == "fusen") 
-        recordArr.push(2);
-      else 
-        recordArr.push(0);
-    }
-  }
-  var rikishiObj = {
-    record: recordArr, 
-    aite: aiteArr
-  }
-  hoshitori.push(rikishiObj);
-}
-
-var tori = document.getElementsByClassName("rb_torikumi"), recordArr = [], aiteArr = [], record;
-for (var i = 0; i < tori[0].children[0].children.length; i++) {
-  record = tori[0].children[0].children[i].children[1].children[0].src.split('_')[1];
-  switch (record) {
-    case "kuro.gif":
-      record = 0; break;
-    case "shiro.gif":
-      record = 1; break;
-    case "fusenpai.gif":
-      record = 2; break;
-    case "fusensho.gif":
-      record = 3; break;
-  }
-  recordArr.push(record);
-  aiteArr.push(tori[0].children[0].children[i].children[3].children[0].innerHTML.split(' ')[1]);
-}
-var rikishiObj = {
-  record: recordArr, 
-  aite: aiteArr
-}
-console.log(rikishiObj);
-*/
