02fa312bcaaa69353181d836c93eaee278907139
John Clyde <john.clyde@mozartdata.com>
2024-02-25 10:21:25 -0800

    Update rankings and fix script to replace records with 0-0-7 so nothing carries over.

--- scrape_to_update_script.js
+++ scrape_to_update_script.js
@@ -1,13 +1,63 @@
+import os
+import re
+
 import requests
 from bs4 import BeautifulSoup
 
-# Fetch the page
-url = "http://sumodb.sumogames.de/gtb/GTBScoreBasho.aspx?b=200005"
-response = requests.get(url)
-soup = BeautifulSoup(response.text, "html.parser")
+
+def update_records(file_path):
+    # Define a pattern to match the record and any awards (e.g., "7-8 Y")
+    pattern = re.compile(r"\d+-\d+(?:-\d+)?(?:\s+[A-Z])?")
+
+    # Start and end flags for theSekitori array
+    start_flag = "var theSekitori = ["
+    end_flag = "];"
+
+    # Flags to track whether we are within the theSekitori array
+    in_theSekitori_array = False
+
+    # Open the original JavaScript file
+    with open(file_path, "r") as file:
+        for line in file:
+            if start_flag in line:
+                in_theSekitori_array = True
+            elif end_flag in line:
+                in_theSekitori_array = False
+
+            if in_theSekitori_array and '"' in line:
+                line = pattern.sub("0-0-7", line)
+            yield line
+
+
+if __name__ == "__main__":
+    file_path = "script.js.new"
+    # Write the modified content back to the file
+    with open(file_path, "w") as file:  # Overwrite the original file
+        for updated_line in update_records("script.js"):
+            file.write(updated_line)
+    print("Records updated successfully.")
+
 
 with open("script.js", "r") as f:
     existing_content = f.readlines()
+
+
+# The filename as saved by wget or to be used as the cache file
+filename = "GTBScoreBasho.aspx?b=200007"
+
+# Check if the file exists locally
+if os.path.exists(filename):
+    # If the file exists, read from the local file
+    with open(filename, 'r') as file:
+        content = file.read()
+else:
+    # If the file does not exist, fetch the page online.
+    url = f"http://sumodb.sumogames.de/gtb/{filename}"
+    response = requests.get(url)
+    content = response.text
+
+# Parse the content using BeautifulSoup
+soup = BeautifulSoup(content, 'html.parser')
 
 updated_content = []
 
