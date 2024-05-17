0e2ed27a9b5e86bc4b2707c76aa375937db6d33b
John Clyde <johnclyde@gmail.com>
2024-05-17 12:57:29 -0700

    Update tableUtils.js with writeTableTitles.js contents

--- tableUtils.js
+++ tableUtils.js
@@ -1,5 +1,38 @@
 "use strict";
 
+export function writeTableTitles(endedBashoDate) {
+  var bashoYear = parseInt(endedBashoDate.substring(0, 4)),
+    bashoMonth = parseInt(endedBashoDate.slice(-2)),
+    tableTitle = document.getElementsByClassName("tableTitle");
+
+  const bashoMonthLookup = {
+      1: "Hatsu",
+      3: "Haru",
+      5: "Natsu",
+      7: "Nagoya",
+      9: "Aki",
+      11: "Kyushu",
+    },
+    getBashoName = (bMonth) => bashoMonthLookup[bMonth];
+
+  tableTitle[0].innerHTML =
+    getBashoName(bashoMonth) +
+    " " +
+    bashoYear +
+    tableTitle[0].innerHTML +
+    " Result";
+  if (bashoMonth > 9) {
+    bashoYear++;
+    bashoMonth = -1;
+  }
+  tableTitle[1].innerHTML =
+    getBashoName(bashoMonth + 2) +
+    " " +
+    bashoYear +
+    " Makuuchi Guess - " +
+    tableTitle[1].innerHTML;
+}
+
 export function addMakushitaTable(theSekitori, sekitoriID) {
   const container = document.querySelectorAll(".banzukeContainer")[1];
   const table1 = document.createElement("table");
