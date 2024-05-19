ae289371310f333668a39d4dccc45bed2a76ad5a
John Clyde <johnclyde@gmail.com>
2024-05-18 21:35:48 -0700

    Update tableUtils.js
    Hold a temporary copy over here.

--- tableUtils.js
+++ tableUtils.js
@@ -2,6 +2,48 @@
 
 import { theSekitori, retiredRikishi, sekitoriID } from "./rikishi.js";
 
+class Rikishi {
+  constructor(rank, name, winCount, id) {
+    this.rank = rank;
+    this.name = name;
+    this.winCount = winCount;
+    this.id = id;
+  }
+
+  getLink() {
+    return `<a href="https://sumodb.sumogames.de/Rikishi.aspx?r=${this.id}" target="_blank">${this.name}</a>`;
+  }
+
+  getRecordLink(basho) {
+    return `<a href="https://sumodb.sumogames.de/Rikishi_basho.aspx?r=${this.id}&b=${basho}" target="_blank">${this.winCount}</a>`;
+  }
+
+  createCard(basho) {
+    const card = document.createElement("div");
+    const wins = this.winCount.split("-")[0];
+    card.id = this.rank;
+    card.className = "redips-drag se";
+    card.setAttribute("data-w", this.rank.startsWith("Ms") || this.rank.startsWith("Sd") || this.rank.startsWith("Jd") || this.rank.startsWith("Jk") ? wins * 2 : wins);
+    card.setAttribute("data-re", this.winCount);
+    card.innerHTML = this.getLink(basho);
+    return card;
+  }
+}
+
+class RetiredRikishi extends Rikishi {
+  constructor(rank, name, winCount, id) {
+    super(rank, name, winCount, id);
+  }
+
+  createCard(basho) {
+    const card = super.createCard(basho);
+    card.style.backgroundColor = "rgb(203, 203, 203)";
+    card.className = "redips-drag intai";
+    card.setAttribute("title", "Retired");
+    return card;
+  }
+}
+
 function writeTableTitles(endedBashoDate) {
   const bashoYear = parseInt(endedBashoDate.substring(0, 4));
   const bashoMonth = parseInt(endedBashoDate.slice(-2));
