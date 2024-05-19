967b00eb809f475eb868759d38e44f69012a60bb
John Clyde <johnclyde@gmail.com>
2024-05-18 22:20:19 -0700

    Clean up redipsUtils.js

--- redipsUtils.js
+++ redipsUtils.js
@@ -1,21 +1,19 @@
-// redipsUtils.js
 "use strict";
 
 export function redipsInit(redips) {
-  let rd = REDIPS.drag;
+  const rd = REDIPS.drag;
   rd.animation = "off";
   rd.init();
   rd.hover.colorTd = "chartreuse";
   rd.only.divClass.se = "b2";
   rd.enableDrag(false, ".intai");
 
-  var radioDrop = document.getElementsByName("dropMode");
+  const radioDrop = document.getElementsByName("dropMode");
 
-  if (radioDrop[2].checked) rd.dropMode = "single";
-  else rd.dropMode = "multiple";
+  rd.dropMode = radioDrop[2].checked ? "single" : "multiple";
 
   rd.event.changed = function (currentCell) {
-    var tooltipCheckbox = document.getElementById("tooltipCheckbox"),
+    const tooltipCheckbox = document.getElementById("tooltipCheckbox"),
       chTooltip = document.createElement("span"),
       change = currentCell.classList.contains("b2")
         ? getChange(rd.obj.id, currentCell.dataset.r)
@@ -23,49 +21,46 @@ export function redipsInit(redips) {
       prevTip = document.getElementById("chTooltip");
 
     if (!tooltipCheckbox.checked) {
-      if (typeof prevTip != "undefined" && prevTip != null) prevTip.remove();
+      if (prevTip) prevTip.remove();
       chTooltip.id = "chTooltip";
-      chTooltip.innerHTML = "(" + rd.obj.id + " " + rd.obj.dataset.re + ")";
-      if (change != "")
-        chTooltip.innerHTML = "<b>" + change + "</b> " + chTooltip.innerHTML;
+      chTooltip.innerHTML = `(${rd.obj.id} ${rd.obj.dataset.re})`;
+      if (change) chTooltip.innerHTML = `<b>${change}</b> ${chTooltip.innerHTML}`;
       rd.obj.prepend(chTooltip);
     }
-    var tip = document.getElementById("tip");
 
-    if (typeof tip != "undefined" && tip != null) tip.remove();
+    const tip = document.getElementById("tip");
+    if (tip) tip.remove();
 
-    var prevShifters = document.getElementsByClassName("shifter"),
+    const prevShifters = document.getElementsByClassName("shifter"),
       prevShiftTo = document.getElementsByClassName("shiftTo");
 
-    if (typeof prevShiftTo[0] != "undefined" && prevShiftTo[0] != null)
-      prevShiftTo[0].classList.remove("shiftTo");
-    if (typeof prevShifters[0] != "undefined" && prevShifters[0] != null) {
-      while (prevShifters.length) prevShifters[0].classList.remove("shifter");
-    }
+    if (prevShiftTo[0]) prevShiftTo[0].classList.remove("shiftTo");
+    while (prevShifters.length) prevShifters[0].classList.remove("shifter");
   };
 
   rd.event.dblClicked = function () {
-    var radioButton = document.getElementsByTagName("input");
-    var rikishiURL = rd.obj.children[0].href;
-    var thisRank = rd.obj.id,
-      originCell = document.querySelectorAll("." + thisRank)[0],
+    const radioButton = document.getElementsByTagName("input");
+    const rikishiURL = rd.obj.children[0].href;
+    const thisRank = rd.obj.id,
+      originCell = document.querySelector(`.${thisRank}`),
       currentCell = rd.findParent("TD", rd.obj);
 
-    if (radioButton[0].checked) window.open(rikishiURL, "_blank").focus();
-    else if (radioButton[1].checked && currentCell.classList.contains("b2")) {
+    if (radioButton[0].checked) {
+      window.open(rikishiURL, "_blank").focus();
+    } else if (radioButton[1].checked && currentCell.classList.contains("b2")) {
       rd.moveObject({
         obj: rd.obj,
         target: originCell,
         callback: function () {
-          if (currentCell.dataset.r.charAt(0) == "J")
+          if (currentCell.dataset.r.charAt(0) === "J") {
             document.getElementById("juRik").innerHTML--;
-          else if (currentCell.dataset.r.startsWith("Ms"))
+          } else if (currentCell.dataset.r.startsWith("Ms")) {
             document.getElementById("msRik").innerHTML--;
-          else document.getElementById("makRik").innerHTML--;
+          } else {
+            document.getElementById("makRik").innerHTML--;
+          }
           originCell.children[0].remove();
-          $("#" + rd.obj.innerText.toLowerCase())
-            .next()
-            .html("");
+          $(`#${rd.obj.innerText.toLowerCase()}`).next().html("");
           updateInfoCells();
           saveBanzuke();
         },
@@ -75,30 +70,30 @@ export function redipsInit(redips) {
   };
 
   rd.event.notMoved = function () {
-    var currentCell = rd.findParent("TD", rd.obj);
-
     // Logic for when the dragged element is not moved
   };
 
   rd.event.droppedBefore = function (targetCell) {
-    var makuCounter = document.getElementById("makRik"),
+    const makuCounter = document.getElementById("makRik"),
       juCounter = document.getElementById("juRik"),
       msCounter = document.getElementById("msRik"),
       thisCard = rd.obj,
       currentCell = rd.findParent("TD", thisCard),
       dropRadio = document.getElementsByName("dropMode");
-    var currentCellRank, targetCellRank;
+    let currentCellRank, targetCellRank;
 
     if (currentCell.classList.contains("b2")) {
       currentCellRank = currentCell.dataset.r.charAt(0);
-      if (currentCellRank == "J") juCounter.innerHTML--;
-      else if (currentCell.dataset.r.startsWith("Ms")) msCounter.innerHTML--;
-      else makuCounter.innerHTML--;
+      if (currentCellRank === "J") {
+        juCounter.innerHTML--;
+      } else if (currentCell.dataset.r.startsWith("Ms")) {
+        msCounter.innerHTML--;
+      } else {
+        makuCounter.innerHTML--;
+      }
     } else if (targetCell.classList.contains("b2")) {
-      var holder = document.createElement("a");
-
-      holder.innerHTML =
-        thisCard.childNodes[thisCard.childNodes.length - 1].innerText;
+      const holder = document.createElement("a");
+      holder.innerHTML = thisCard.childNodes[thisCard.childNodes.length - 1].innerText;
       holder.href = thisCard.children[thisCard.childNodes.length - 1].href;
       holder.target = "_blank";
       currentCell.appendChild(holder);
@@ -106,58 +101,39 @@ export function redipsInit(redips) {
 
     if (targetCell.classList.contains("b2")) {
       targetCellRank = targetCell.dataset.r.charAt(0);
-      if (targetCellRank == "J") juCounter.innerHTML++;
-      else if (targetCell.dataset.r.startsWith("Ms")) msCounter.innerHTML++;
-      else makuCounter.innerHTML++;
+      if (targetCellRank === "J") {
+        juCounter.innerHTML++;
+      } else if (targetCell.dataset.r.startsWith("Ms")) {
+        msCounter.innerHTML++;
+      } else {
+        makuCounter.innerHTML++;
+      }
     } else {
       targetCell.children[0].remove();
-      if (rd.obj.children.length > 1)
-        $("#" + rd.obj.children[1].innerText.toLowerCase())
-          .next()
-          .html("");
-      else
-        $("#" + rd.obj.innerText.toLowerCase())
-          .next()
-          .html("");
+      const nextCell = rd.obj.children.length > 1 ? rd.obj.children[1] : rd.obj;
+      $(`#${nextCell.innerText.toLowerCase()}`).next().html("");
     }
 
-    if (
-      dropRadio[1].checked &&
-      targetCell !== currentCell &&
-      targetCell.classList.contains("b2") &&
-      targetCell.children.length > 0
-    ) {
-      var b2Cell = document.querySelectorAll(".b2"),
-        targetIndex = Array.prototype.slice.call(b2Cell).indexOf(targetCell);
+    if (dropRadio[1].checked && targetCell !== currentCell && targetCell.classList.contains("b2") && targetCell.children.length > 0) {
+      const b2Cell = document.querySelectorAll(".b2"),
+        targetIndex = Array.prototype.indexOf.call(b2Cell, targetCell);
 
       if (targetIndex >= 0) {
-        for (var i = targetIndex; i < b2Cell.length; i++) {
-          if (
-            b2Cell[i].children.length == 0 ||
-            targetIndex == 53 ||
-            targetIndex == 81 ||
-            targetIndex == 111 ||
-            (b2Cell[i].children.length == 1 && b2Cell[i] === thisCard.parentNode) ||
-            ((i == 53 || i == 81 || i == 111) && b2Cell[i].children.length > 0)
-          ) {
-            for (var j = i - 1; j >= targetIndex; i--, j--)
+        for (let i = targetIndex; i < b2Cell.length; i++) {
+          if (b2Cell[i].children.length === 0 || [53, 81, 111].includes(targetIndex)) {
+            for (let j = i - 1; j >= targetIndex; i--, j--) {
               rd.relocate(b2Cell[j], b2Cell[i], "instant");
+            }
             redips.init();
             break;
           }
         }
       } else {
-        for (var i = targetIndex; i >= 0; i--) {
-          if (
-            b2Cell[i].children.length == 0 ||
-            targetIndex == 0 ||
-            targetIndex == 54 ||
-            targetIndex == 82 ||
-            (b2Cell[i].children.length == 1 && b2Cell[i] === thisCard.parentNode) ||
-            ((i == 0 || i == 54 || i == 82) && b2Cell[i].children.length > 0)
-          ) {
-            for (var j = i + 1; j <= targetIndex; i++, j++)
+        for (let i = targetIndex; i >= 0; i--) {
+          if (b2Cell[i].children.length === 0 || [0, 54, 82].includes(targetIndex)) {
+            for (let j = i + 1; j <= targetIndex; i++, j++) {
               rd.relocate(b2Cell[j], b2Cell[i], "instant");
+            }
             redips.init();
             break;
           }
@@ -166,14 +142,14 @@ export function redipsInit(redips) {
     }
 
     if (rd.obj.childNodes.length > 1) {
-      for (var i = 0; i < rd.obj.childNodes.length; i++)
-        rd.obj.removeChild(rd.obj.childNodes[i]);
+      while (rd.obj.childNodes.length > 1) {
+        rd.obj.removeChild(rd.obj.childNodes[1]);
+      }
     }
   };
 
   rd.event.dropped = function (targetCell) {
-    if (targetCell.style.backgroundColor != "")
-      targetCell.style.backgroundColor = "";
+    targetCell.style.backgroundColor = "";
     updateInfoCells();
     showSaving();
   };
@@ -184,11 +160,11 @@ export function redipsInit(redips) {
 }
 
 export function redipsResetBanzuke() {
-  if (confirm("Reset the banzuke?") == true) {
-    var redipsCell = document.querySelectorAll(".redips-only"),
+  if (confirm("Reset the banzuke?")) {
+    const redipsCell = document.querySelectorAll(".redips-only"),
       b2Cell = document.querySelectorAll(".b2"),
       chgCell = document.getElementsByClassName("ch");
-    var c1 = document.querySelectorAll(".new"),
+    const c1 = document.querySelectorAll(".new"),
       c2 = document.querySelectorAll(".ch1"),
       c3 = document.querySelectorAll(".rs2"),
       c4 = document.querySelectorAll(".cur"),
@@ -197,16 +173,17 @@ export function redipsResetBanzuke() {
 
     window.localStorage.removeItem("savedBanzuke");
     document.getElementById("makRik").innerHTML = 0;
-    for (var i = 1; i < 8; i++)
-      window.localStorage.removeItem("colCheck" + String(i));
+    for (let i = 1; i < 8; i++) {
+      window.localStorage.removeItem(`colCheck${i}`);
+    }
 
-    for (var i = 0; i < b2Cell.length; i++) {
-      if (b2Cell[i].children.length > 0) {
-        for (var j = b2Cell[i].children.length - 1; j >= 0; j--) {
-          for (var k = 0; k < 100; k++) {
-            if (redipsCell[k].classList.contains(b2Cell[i].children[j].id)) {
+    b2Cell.forEach((cell) => {
+      if (cell.children.length > 0) {
+        for (let j = cell.children.length - 1; j >= 0; j--) {
+          for (let k = 0; k < 100; k++) {
+            if (redipsCell[k].classList.contains(cell.children[j].id)) {
               rd.moveObject({
-                obj: b2Cell[i].children[j],
+                obj: cell.children[j],
                 target: redipsCell[k],
               });
               redipsCell[k].children[0].style.display = "none";
@@ -215,63 +192,71 @@ export function redipsResetBanzuke() {
           }
         }
       }
-    }
-    for (var i = 2; i < c1.length; i++) {
-      if (c1[i].innerHTML != "") {
+    });
+
+    for (let i = 2; i < c1.length; i++) {
+      if (c1[i].innerHTML !== "") {
         c1[i].innerHTML = "";
         c2[i].innerHTML = "";
       }
     }
-    for (var i = 2; i < c3.length; i++) {
-      if (c3[i].innerHTML != "") {
+    for (let i = 2; i < c3.length; i++) {
+      if (c3[i].innerHTML !== "") {
         c3[i].innerHTML = "";
         c4[i].innerHTML = "";
         c5[i].innerHTML = "";
       }
-      if (c6[i].children[0].innerHTML != "") c6[i].children[0].innerHTML = "";
+      if (c6[i].children[0].innerHTML !== "") {
+        c6[i].children[0].innerHTML = "";
+      }
     }
     location.reload();
   }
 }
 
 export function redipsArrange() {
-  if (confirm("Confirm auto-arrange?") == true) {
-    var rikishi = document.querySelectorAll(".se"),
+  if (confirm("Confirm auto-arrange?")) {
+    const rikishi = document.querySelectorAll(".se"),
       msCounter = document.getElementById("msRik"),
       juCounter = document.getElementById("juRik"),
       makuCounter = document.getElementById("makRik");
 
-    for (var i = 0; i < rikishi.length; i++) {
-      var rikishiRank = rikishi[i].id;
+    rikishi.forEach((rikishi) => {
+      const rikishiRank = rikishi.id;
 
       if (
         (rikishiRank.startsWith("Ms") && rikishiRank.slice(2, -1) > 15) ||
         rikishiRank.startsWith("Sd")
       ) {
-        continue;
+        return;
       }
-      if (!rikishi[i].parentNode.classList.contains("b2")) {
-        var holder = document.createElement("a");
-
-        holder.innerHTML = rikishi[i].innerText;
-        holder.href = rikishi[i].children[0].href;
+      if (!rikishi.parentNode.classList.contains("b2")) {
+        const holder = document.createElement("a");
+        holder.innerHTML = rikishi.innerText;
+        holder.href = rikishi.children[0].href;
         holder.target = "_blank";
-        rikishi[i].parentNode.appendChild(holder);
+        rikishi.parentNode.appendChild(holder);
       } else {
-        if (rikishi[i].parentNode.dataset.r.startsWith("J"))
+        if (rikishi.parentNode.dataset.r.startsWith("J")) {
           juCounter.innerHTML--;
-        else if (rikishi[i].parentNode.dataset.r.startsWith("Ms"))
+        } else if (rikishi.parentNode.dataset.r.startsWith("Ms")) {
           msCounter.innerHTML--;
-        else makuCounter.innerHTML--;
+        } else {
+          makuCounter.innerHTML--;
+        }
       }
-      if (rikishiRank.startsWith("J")) juCounter.innerHTML++;
-      else if (rikishiRank.startsWith("Ms")) msCounter.innerHTML++;
-      else makuCounter.innerHTML++;
+      if (rikishiRank.startsWith("J")) {
+        juCounter.innerHTML++;
+      } else if (rikishiRank.startsWith("Ms")) {
+        msCounter.innerHTML++;
+      } else {
+        makuCounter.innerHTML++;
+      }
       rd.moveObject({
-        obj: rikishi[i],
-        target: document.querySelector('[data-r="' + rikishiRank + '"]'),
+        obj: rikishi,
+        target: document.querySelector(`[data-r="${rikishiRank}"]`),
       });
-    }
+    });
     updateInfoCells();
   }
 }
