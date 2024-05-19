a3aae2e7441b009053810bf05c5bcc2cd4921470
John Clyde <johnclyde@gmail.com>
2024-05-18 22:14:14 -0700

    Clean up banzukeUtils.js

--- banzukeUtils.js
+++ banzukeUtils.js
@@ -1,229 +1,143 @@
-// banzukeUtils.js
 "use strict";
 
 export function toggleColumns(button) {
-  var column = button.value;
-  var colCell = document.getElementsByClassName(column);
-  var colCheck = document.querySelectorAll(".columnCheckbox");
-  var tableTitle = document.querySelectorAll(".tableTitle");
+  const column = button.value;
+  const colCells = document.getElementsByClassName(column);
+  const colChecks = document.querySelectorAll(".columnCheckbox");
+  const tableTitles = document.querySelectorAll(".tableTitle");
+
+  const updateColSpan = (index, value) => {
+    tableTitles[index].colSpan += value;
+  };
 
   if (button.checked) {
-    if (button.classList.contains("forB1")) tableTitle[0].colSpan += 2;
-    else {
-      tableTitle[1].colSpan += 2;
-      tableTitle[2].colSpan += 2;
-    }
-    for (var i = 0; i < colCell.length; i++) colCell[i].classList.remove("hid");
+    button.classList.contains("forB1") ? updateColSpan(0, 2) : updateColSpan(1, 2) & updateColSpan(2, 2);
+    Array.from(colCells).forEach(cell => cell.classList.remove("hid"));
   } else {
-    if (button.classList.contains("forB1")) tableTitle[0].colSpan -= 2;
-    else {
-      tableTitle[1].colSpan -= 2;
-      tableTitle[2].colSpan -= 2;
-    }
-    for (var i = 0; i < colCell.length; i++) colCell[i].classList.add("hid");
+    button.classList.contains("forB1") ? updateColSpan(0, -2) : updateColSpan(1, -2) & updateColSpan(2, -2);
+    Array.from(colCells).forEach(cell => cell.classList.add("hid"));
   }
-  for (var i = 1; i < 8; i++) {
-    window.localStorage.setItem(
-      "colCheck" + String(i),
-      colCheck[i - 1].checked,
-    );
-  }
+
+  colChecks.forEach((check, i) => {
+    window.localStorage.setItem("colCheck" + (i + 1), check.checked);
+  });
+
   saveBanzuke();
 }
 
 export function updateInfoCells() {
-  var b2Cell = document.querySelectorAll(".b2"),
-    b1Cell = document
-      .getElementById("banzuke1")
-      .querySelectorAll(".redips-only"),
-    originCell,
-    newRankCell,
-    b1Chg,
-    resultLink,
-    resultCell,
-    currRankCell,
-    targetChgCell;
+  const b2Cells = document.querySelectorAll(".b2");
+  const b1Cells = document.getElementById("banzuke1").querySelectorAll(".redips-only");
 
-  for (var i = 0; i < b1Cell.length; i++) {
-    if (
-      b1Cell[i].children.length > 0 &&
-      b1Cell[i].children[0].tagName == "DIV"
-    ) {
-      newRankCell = b1Cell[i].nextSibling;
-      if (i % 2 != 0) {
-        newRankCell = newRankCell.nextSibling;
-      }
-      if (newRankCell.innerHTML != "") {
-        newRankCell.innerHTML = "";
-        b1Chg = newRankCell.nextSibling;
-        b1Chg.innerHTML = "";
-      }
+  const clearCells = cell => {
+    const newRankCell = cell.nextSibling;
+    if (newRankCell) {
+      newRankCell.innerHTML = "";
+      const b1Chg = newRankCell.nextSibling;
+      if (b1Chg) b1Chg.innerHTML = "";
     }
-  }
+  };
 
-  for (var i = 0; i < b2Cell.length; i++) {
-    resultCell = b2Cell[i].nextSibling;
-    currRankCell = b2Cell[i].previousSibling;
-    targetChgCell = resultCell.nextSibling;
+  b1Cells.forEach(cell => {
+    if (cell.children.length > 0 && cell.children[0].tagName === "DIV") clearCells(cell);
+  });
 
-    if (b2Cell[i].children.length > 0) {
-      for (var j = 0; j < b2Cell[i].children.length; j++) {
-        var thisRank = b2Cell[i].children[j].id,
-          rikishiWins = b2Cell[i].children[j].dataset.w,
-          thisChg,
-          targetCellRank,
-          chg;
+  b2Cells.forEach(b2Cell => {
+    const resultCell = b2Cell.nextSibling;
+    const currRankCell = b2Cell.previousSibling;
+    const targetChgCell = resultCell.nextSibling;
 
-        originCell = document.querySelectorAll("." + thisRank)[0];
-        newRankCell = originCell.nextSibling;
+    if (b2Cell.children.length > 0) {
+      Array.from(b2Cell.children).forEach((child, j) => {
+        const thisRank = child.id;
+        const rikishiWins = child.dataset.w;
+        const originCell = document.querySelector(`.${thisRank}`);
+        let newRankCell = originCell.nextSibling;
+        let resultLink;
+
         if (thisRank.endsWith("w")) {
           newRankCell = newRankCell.nextSibling;
           resultLink = originCell.nextSibling.innerHTML;
-        } else resultLink = originCell.previousSibling.innerHTML;
-
-        targetCellRank = b2Cell[i].dataset.r;
-
-        thisChg = getChange(thisRank, targetCellRank);
-
-        if (thisRank.startsWith("Ms") || thisRank.startsWith("Sd")) {
-          if (thisRank.endsWith("TD")) thisRank = thisRank.slice(0, -2);
-          thisChg =
-            '<a href="https://sumodb.sumogames.de/Query.aspx?show_form=0&form1_rank=' +
-            thisRank +
-            "&form1_wins=" +
-            rikishiWins / 2 +
-            "&form1_year=196007-now&form2_highlight=on&form2_rank=" +
-            targetCellRank +
-            '" target="_blank" title="Click to run a SumoDB query">' +
-            thisChg +
-            "</a>";
         } else {
-          thisChg =
-            '<a href="https://sumodb.sumogames.de/Query.aspx?show_form=0&form1_rank=' +
-            thisRank +
-            "&form1_wins=" +
-            rikishiWins +
-            "&form1_year=193905-194401,194905-now&form2_highlight=on&form2_rank=" +
-            targetCellRank +
-            '" target="_blank" title="Click to run a SumoDB query">' +
-            thisChg +
-            "</a>";
+          resultLink = originCell.previousSibling.innerHTML;
         }
 
-        newRankCell.innerHTML = b2Cell[i].dataset.r;
+        const targetCellRank = b2Cell.dataset.r;
+        const thisChg = getChange(thisRank, targetCellRank, rikishiWins);
 
-        b1Chg = newRankCell.nextSibling;
+        newRankCell.innerHTML = targetCellRank;
+        const b1Chg = newRankCell.nextSibling;
         b1Chg.innerHTML = thisChg;
 
-        if (j == 0) {
+        if (j === 0) {
           targetChgCell.innerHTML = thisChg;
           resultCell.innerHTML = resultLink;
-          currRankCell.innerHTML =
-            "<span>" + b2Cell[i].children[j].id + "</span>";
+          currRankCell.innerHTML = `<span>${thisRank}</span>`;
         } else {
-          targetChgCell.innerHTML += "<br>" + thisChg;
-          resultCell.innerHTML += "<br>" + resultLink;
-          currRankCell.innerHTML +=
-            "<br><span>" + b2Cell[i].children[j].id + "</span>";
+          targetChgCell.innerHTML += `<br>${thisChg}`;
+          resultCell.innerHTML += `<br>${resultLink}`;
+          currRankCell.innerHTML += `<br><span>${thisRank}</span>`;
         }
-        if (thisRank.startswith("Ms"))
-          $("#" + b2Cell[i].children[j].innerText.toLowerCase())
-            .next()
-            .html(thisChg);
 
-        var rikishiBgColor = window
-          .getComputedStyle(b2Cell[i].children[j])
-          .getPropertyValue("background-color");
-
+        const rikishiBgColor = window.getComputedStyle(child).getPropertyValue("background-color");
         currRankCell.children[j * 2].style.background = rikishiBgColor;
         resultCell.children[j * 2].style.background = rikishiBgColor;
-      }
+      });
     } else {
       resultCell.innerHTML = "";
       currRankCell.innerHTML = "";
       targetChgCell.innerHTML = "";
     }
-  }
+  });
 }
 
-export function getChange(thisRank, targetCellRank) {
-  var chg;
+function getChange(thisRank, targetCellRank, rikishiWins) {
+  const changeMatrix = [
+    ["calc", "!!!", "!!!", "!!!", "!!!", "!!!", "!!!", "!!!"],
+    [" ↑ ", "calc", " ↓ ", "!!!", "!!!", "!!!", "!!!", "!!!"],
+    ["!!!", " ↑ ", "calc", " ↓ ", " ↓ ", "!!!", "!!!", "!!!"],
+    ["!!!", "!!!", " ↑ ", "calc", " ↓ ", "!!!", "!!!", "!!!"],
+    ["!!!", "!!!", " ↑ ", " ↑ ", "calc", " ↓ ", "!!!", "!!!"],
+    ["!!!", "!!!", "!!!", "!!!", " ↑ ", "calc", " ↓ ", "!!!"],
+    ["!!!", "!!!", "!!!", "!!!", "!!!", " ↑ ", "calc", " ↓ "],
+    ["!!!", "!!!", "!!!", "!!!", "!!!", "!!!", " ↑ ", "calc"],
+  ];
 
-  if (thisRank == targetCellRank) chg = "─";
-  else {
-    const change = [
-      ["calc", "!!!", "!!!", "!!!", "!!!", "!!!", "!!!", "!!!"],
-      [" ↑ ", "calc", " ↓ ", "!!!", "!!!", "!!!", "!!!", "!!!"],
-      ["!!!", " ↑ ", "calc", " ↓ ", " ↓ ", "!!!", "!!!", "!!!"],
-      ["!!!", "!!!", " ↑ ", "calc", " ↓ ", "!!!", "!!!", "!!!"],
-      ["!!!", "!!!", " ↑ ", " ↑ ", "calc", " ↓ ", "!!!", "!!!"],
-      ["!!!", "!!!", "!!!", "!!!", " ↑ ", "calc", " ↓ ", "!!!"],
-      ["!!!", "!!!", "!!!", "!!!", "!!!", " ↑ ", "calc", " ↓ "],
-      ["!!!", "!!!", "!!!", "!!!", "!!!", "!!!", " ↑ ", "calc"],
-    ];
-    var r1, r2;
+  const rankMap = {
+    "Y": 0,
+    "O": 1,
+    "S": 2,
+    "K": 3,
+    "M": 4,
+    "J": 5,
+    "Ms": 6,
+    "Sd": 7
+  };
 
-    switch (thisRank.charAt(0)) {
-      case "Y":
-        r1 = 0;
-        break;
-      case "O":
-        r1 = 1;
-        break;
-      case "S":
-        if (!thisRank.startsWith("Sd")) r1 = 2;
-        else r1 = 7;
-        break;
-      case "K":
-        r1 = 3;
-        break;
-      case "M":
-        if (!thisRank.startsWith("Ms")) r1 = 4;
-        else r1 = 6;
-        break;
-      default:
-        r1 = 5;
-    }
-    switch (targetCellRank.charAt(0)) {
-      case "Y":
-        r2 = 0;
-        break;
-      case "O":
-        r2 = 1;
-        break;
-      case "S":
-        if (!targetCellRank.startsWith("Sd")) r2 = 2;
-        else r2 = 7;
-        break;
-      case "K":
-        r2 = 3;
-        break;
-      case "M":
-        if (!targetCellRank.startswith("Ms")) r2 = 4;
-        else r2 = 6;
-        break;
-      default:
-        r2 = 5;
-    }
+  let r1 = rankMap[thisRank[0]];
+  let r2 = rankMap[targetCellRank[0]];
 
-    if (change[r1][r2] != "calc") chg = change[r1][r2];
-    else {
-      var thisRankNum =
-          r1 == 6 || r1 == 7
-            ? parseInt(thisRank.slice(2, -1))
-            : parseInt(thisRank.slice(1, -1)),
-        targetRankNum =
-          r2 == 6 || r2 == 7
-            ? parseInt(targetCellRank.slice(2, -1))
-            : parseInt(targetCellRank.slice(1, -1));
+  if (thisRank.startsWith("Ms") || thisRank.startsWith("Sd")) r1 = rankMap[thisRank.slice(0, 2)];
+  if (targetCellRank.startsWith("Ms") || targetCellRank.startsWith("Sd")) r2 = rankMap[targetCellRank.slice(0, 2)];
 
-      if (thisRank.slice(-1) == "w") thisRankNum += 0.5;
-      if (targetCellRank.slice(-1) == "w") targetRankNum += 0.5;
+  let chg = changeMatrix[r1][r2];
 
-      chg = thisRankNum - targetRankNum;
+  if (chg === "calc") {
+    let thisRankNum = parseInt(thisRank.slice(1), 10);
+    let targetRankNum = parseInt(targetCellRank.slice(1), 10);
 
-      if (chg > 0) chg = "+" + chg;
-    }
+    if (thisRank.endsWith("w")) thisRankNum += 0.5;
+    if (targetCellRank.endsWith("w")) targetRankNum += 0.5;
+
+    chg = thisRankNum - targetRankNum;
+    if (chg > 0) chg = `+${chg}`;
+  }
+
+  if (thisRank.startsWith("Ms") || thisRank.startsWith("Sd")) {
+    if (thisRank.endsWith("TD")) thisRank = thisRank.slice(0, -2);
+    chg = `<a href="https://sumodb.sumogames.de/Query.aspx?show_form=0&form1_rank=${thisRank}&form1_wins=${rikishiWins / 2}&form1_year=196007-now&form2_highlight=on&form2_rank=${targetCellRank}" target="_blank" title="Click to run a SumoDB query">${chg}</a>`;
+  } else {
+    chg = `<a href="https://sumodb.sumogames.de/Query.aspx?show_form=0&form1_rank=${thisRank}&form1_wins=${rikishiWins}&form1_year=193905-194401,194905-now&form2_highlight=on&form2_rank=${targetCellRank}" target="_blank" title="Click to run a SumoDB query">${chg}</a>`;
   }
 
   return chg;
