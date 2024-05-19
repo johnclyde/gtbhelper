3fc933ae54e0d33790bd6ae7c3a5f8b9749c3daf
John Clyde <johnclyde@gmail.com>
2024-05-18 20:57:22 -0700

    Ask GPT to clean up script.js

--- script.js
+++ script.js
@@ -1,78 +1,59 @@
 "use strict";
 
 import { exportTableToCSV, darkmode, nodark } from "./utilities.js";
-import { saveToLocalStorage, getFromLocalStorage, removeFromLocalStorage, initializeLocalStorage, saveRadioState, saveDropRadioState } from "./localStorageManager.js";
+import { initializeLocalStorage } from "./localStorageManager.js";
 import { writeTableTitles, addMakushitaTable } from "./tableUtils.js";
 import { theSekitori, retiredRikishi, sekitoriID, addRikishi } from "./rikishi.js";
 import { displayDrafts, setupDraftEventHandlers } from "./drafts.js";
-import { toggleColumns, updateInfoCells, getChange } from "./banzukeUtils.js";
-import { redipsInit, redipsResetBanzuke, redipsArrange } from "./redipsUtils.js";
+import { updateInfoCells } from "./banzukeUtils.js";
+import { redipsInit } from "./redipsUtils.js";
 
-window.onload = function () {
-  var basho = "202401"; // The date of the basho just ended
+window.onload = () => {
+  const basho = "202401";
 
-  // This must be a hyperlink
-  $("#exportToCsv1").on("click", function (event) {
-    exportTableToCSV.apply(this, [$("#banzuke1"), "banzuke1.csv"]);
+  $("#exportToCsv1").on("click", () => {
+    exportTableToCSV($("#banzuke1"), "banzuke1.csv");
   });
-  $("#exportToCsv2").on("click", function (event) {
-    exportTableToCSV.apply(this, [$("#banzuke2"), "banzuke2.csv"]);
+  $("#exportToCsv2").on("click", () => {
+    exportTableToCSV($("#banzuke2"), "banzuke2.csv");
   });
 
   initializeLocalStorage();
-  if (window.localStorage.getItem("savedBanzuke") === null) {
+
+  if (!window.localStorage.getItem("savedBanzuke")) {
     writeTableTitles(basho);
     addRikishi(basho, theSekitori, sekitoriID, retiredRikishi);
     addMakushitaTable(theSekitori, sekitoriID);
   }
 
-  var noteCells = document.querySelectorAll(".nte");
-
-  for (var i = 2; i < noteCells.length; i++) {
-    let time = 0;
-    noteCells[i].children[0].contentEditable = "true";
-    noteCells[i].children[0].addEventListener("input", function () {
-      // Reset the timer
-      clearTimeout(time);
-
-      time = setTimeout(function () {
-        saveBanzuke();
-        showSaving();
-      }, 1000);
-    });
-  }
-
-  var checkbox = document.getElementById("ChangeTheme"); //get the checkbox to a variable
-
-  //check storage if dark mode was on or off
-  if (localStorage.getItem("mode") == "dark") {
-    darkmode(); //if dark mode was on, run this funtion
-  } else {
-    nodark(); //else run this funtion
-  }
-
-  checkbox.addEventListener("change", function () {
-    //check if the checkbox is checked or not
-    if (checkbox.checked) {
-      darkmode(); //if the checkbox is checked, run this funtion
-    } else {
-      nodark(); //else run this funtion
+  document.querySelectorAll(".nte").forEach((noteCell, index) => {
+    if (index >= 2) {
+      noteCell.children[0].contentEditable = "true";
+      noteCell.children[0].addEventListener("input", debounce(saveBanzuke, 1000));
     }
+  });
+
+  const checkbox = document.getElementById("ChangeTheme");
+  
+  if (localStorage.getItem("mode") === "dark") {
+    darkmode();
+  } else {
+    nodark();
+  }
+
+  checkbox.addEventListener("change", () => {
+    checkbox.checked ? darkmode() : nodark();
     updateInfoCells();
   });
 
-  if (window.localStorage.getItem("colCheck1") === null) {
-    var columnCheckbox = document.querySelectorAll(".checkedByDefault");
-
-    for (var i = 0; i < columnCheckbox.length; i++)
-      columnCheckbox[i].checked = true;
+  if (!window.localStorage.getItem("colCheck1")) {
+    document.querySelectorAll(".checkedByDefault").forEach(checkbox => {
+      checkbox.checked = true;
+    });
   } else {
-    for (var i = 1; i < 8; i++) {
-      var columnCheck = document.querySelectorAll(".columnCheckbox")[i - 1];
-      var check = JSON.parse(
-        window.localStorage.getItem("colCheck" + String(i)),
-      );
-
+    for (let i = 1; i <= 8; i++) {
+      const columnCheck = document.querySelectorAll(".columnCheckbox")[i - 1];
+      const check = JSON.parse(window.localStorage.getItem(`colCheck${i}`));
       columnCheck.checked = check;
     }
   }
@@ -81,27 +62,33 @@ window.onload = function () {
   setupDraftEventHandlers();
 };
 
-if (window.addEventListener)
-  window.addEventListener("load", () => redipsInit(redips), false);
-else if (window.attachEvent) window.attachEvent("onload", () => redipsInit(redips));
+window.addEventListener("load", () => redipsInit(redips));
 
-document.addEventListener('DOMContentLoaded', function() {
-    const banzuke1Config = [
-        { prefix: 'M', range: Array.from({length: 17}, (_, i) => i + 1) },
-        { divider: true },
-        { prefix: 'J', range: Array.from({length: 14}, (_, i) => i + 1) },
-        { divider: true },
-        { prefix: 'Ms', range: Array.from({length: 60}, (_, i) => i + 1) }
-    ];
+document.addEventListener('DOMContentLoaded', () => {
+  const banzuke1Config = [
+    { prefix: 'M', range: Array.from({ length: 17 }, (_, i) => i + 1) },
+    { divider: true },
+    { prefix: 'J', range: Array.from({ length: 14 }, (_, i) => i + 1) },
+    { divider: true },
+    { prefix: 'Ms', range: Array.from({ length: 60 }, (_, i) => i + 1) }
+  ];
 
-    const banzuke2Config = [
-        { prefix: 'M', range: Array.from({length: 18}, (_, i) => i + 1) },
-        { divider: 'Juryo Guess - <span id="juRik">0</span>/28' },
-        { prefix: 'J', range: Array.from({length: 14}, (_, i) => i + 1) },
-        { divider: 'Makushita Joi Guess - <span id="msRik">0</span>/30' },
-        { prefix: 'Ms', range: Array.from({length: 60}, (_, i) => i + 1) }
-    ];
+  const banzuke2Config = [
+    { prefix: 'M', range: Array.from({ length: 18 }, (_, i) => i + 1) },
+    { divider: 'Juryo Guess - <span id="juRik">0</span>/28' },
+    { prefix: 'J', range: Array.from({ length: 14 }, (_, i) => i + 1) },
+    { divider: 'Makushita Joi Guess - <span id="msRik">0</span>/30' },
+    { prefix: 'Ms', range: Array.from({ length: 60 }, (_, i) => i + 1) }
+  ];
 
-    populateBanzukeTable('banzuke1Body', banzuke1Config, createRowBanzuke1);
-    populateBanzukeTable('banzuke2Body', banzuke2Config, createRowBanzuke2);
+  populateBanzukeTable('banzuke1Body', banzuke1Config, createRowBanzuke1);
+  populateBanzukeTable('banzuke2Body', banzuke2Config, createRowBanzuke2);
 });
+
+function debounce(func, wait) {
+  let timeout;
+  return function (...args) {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => func.apply(this, args), wait);
+  };
+}
